616d1aeff9f8be972f9a44221403a8284dbdf209
Hazel Tate <void.iros@gmail.com>
2022-06-12 15:29:49 -0500

    worked on routes

--- Models/Posts.js
+++ Models/Posts.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongooose.Schema;
 
 const PostSchema = new Schema({
-  like: { type: Number, default: true },
+  like: { type: Number, default: 0 },
   users: [String],
+  pageNumber: { type: Number },
 });
 
 module.exports = mongoose.model('Post', PostSchema);
--- controllers/post.js
+++ controllers/post.js
@@ -5,3 +5,18 @@ module.exports.getPost = async (req, res) => {
   const post = await Post.findById(postId);
   res.json(post);
 };
+
+module.exports.createPost = async (req, res) => {
+  const like = 0;
+  const { user, postNumber, pageNumber } = req.body;
+  const newPost = await new Post(user, postNumber, pageNumber, like);
+  res.send(newPost);
+};
+
+module.exports.updatePost = async (req, res) => {
+  const { number } = req.body;
+  const postId = req.params;
+  const post = await Post.findByIdAndUpdate(postId, { likes: likes - numbers });
+  await post.save();
+  res.json(post.likes);
+};
