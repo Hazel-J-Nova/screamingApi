e2185a627eebeb18dfdd745ef935f1f35bdd94bb
Hazel Tate <void.iros@gmail.com>
2022-06-14 17:31:28 -0500

    finishing up basic routes

--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,11 @@
-const {Router} = require('express');
+const { Router } = require('express');
+const post = require('../controllers/post');
+const catchAsync = require('../utils/catchAsync');
 
+Router.route('/post/:postId')
+  .get(catchAsync, post.getPost)
+  .post(catchAsync, post.createPost)
+  .put(catchAsync, post.updatePost)
+  .delete(catchAsync, post.deletePost);
 
-Router.route('/post/:postId').get()
+module.exports.Router;
